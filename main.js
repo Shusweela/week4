c748ee9347238328dcce06264e1628a4eb6bf4de
shusweela <shusweela@gmailcom>
2020-07-27 15:07:05 -0500

    added comments of answes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //setting variables
-var text = "multiply",
-num = 3;
+var text = "multiply";
+var num = 3;
 
-//multiplyig the string and number variable
+//multiplyig the string and number variable (results of nan)
 console.log(text*num);
 
-//console log adding 2 and true
+//console log adding 2 and true (results of  null or undefined)
 console.log(2+true);
