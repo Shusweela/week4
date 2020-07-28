89bae411439a30960922b2ee10be2906bccbe28b
shusweela <shusweela@gmailcom>
2020-07-28 08:59:16 -0500

    fixing homework

---
+++ main.js
@@ -0,0 +1,9 @@
+//setting variables
+var text = "multiply";
+var num = 3;
+
+//multiplyig the string and number variable (results of nan)
+console.log(text*num);
+
+//console log adding 2 and true (results of  null or undefined)
+console.log(2+true);
