ae90b46a15f2a2e069ad13238346f5d01001df73
shusweela <shusweela@gmailcom>
2020-07-29 23:19:48 -0500

    homework 5_2

--- main.js
+++ main.js
@@ -7,3 +7,39 @@ console.log(text*num);
 
 //console log adding 2 and true (results of  null or undefined)
 console.log(2+true);
+
+    //Homework 4_2--using math
+    var counter = 2;
+    var add = counter + 5;
+
+//console log multiplying sum to 2 (7*2=14)
+   console.log(add*2);
+
+//the answer to the previous console log module by 3 (should be remainder 2)
+console.log(14 % 3); //I guessed correctly--yey!
+
+
+
+//New Math operation to test
+var pink = 5;
+
+//determines if the number is even and odd and gives the output and answer
+Function evenOdd(num) {
+    if num % 2 = 0,
+    console.log(num % 2 + " -- " + "even");
+    else if num % > 0,
+    console.log(num % 2 + " -- " + "odd");
+    else 
+    console.log("error")
+}
+
+??
+
+}
+if pink % 2 = 0, 
+console.log ("This is an even number"),
+
+else console.log("This number is odd")
+
+
+    Counter % 3
